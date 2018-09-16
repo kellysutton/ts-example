3b9cce06b98582b3b0309f4ee55144fcc42cef8f
Kelly Sutton <michael.k.sutton@gmail.com>
2018-09-15 17:09:21 -0700

    Simple parcel-typescript example

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function App() {
+  return (
+    <div className="container" data-testid="toggle-container">
+      <p>Hello, Typescript!</p>
+    </div>
+  );
+}
+
+export default App
